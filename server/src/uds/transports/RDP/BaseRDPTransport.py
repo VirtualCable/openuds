60556ad4ec158dd8f6799b6fd3523f888ff54850
Adolfo Gómez García <dkmaster@dkmon.com>
2015-04-21 12:29:10 +0200

    fixed RDP with user credentials

--- server/src/uds/transports/RDP/BaseRDPTransport.py
+++ server/src/uds/transports/RDP/BaseRDPTransport.py
@@ -90,7 +90,7 @@ class BaseRDPTransport(Transport):
     def processUserPassword(self, service, user, password):
         username = user.getUsernameForAuth()
 
-        if self.fixedName.value is not '':
+        if self.fixedName.value != '':
             username = self.fixedName.value
 
         proc = username.split('@')
@@ -100,9 +100,9 @@ class BaseRDPTransport(Transport):
             domain = ''
         username = proc[0]
 
-        if self.fixedPassword.value is not '':
+        if self.fixedPassword.value != '':
             password = self.fixedPassword.value
-        if self.fixedDomain.value is not '':
+        if self.fixedDomain.value != '':
             domain = self.fixedDomain.value
         if self.useEmptyCreds.isTrue():
             username, password, domain = '', '', ''
