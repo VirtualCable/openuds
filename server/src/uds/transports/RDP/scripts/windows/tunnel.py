cfded5856388fa99a4214b8cf3f80c214176ad96
Adolfo Gómez García <dkmaster@dkmon.com>
2017-05-19 19:36:07 +0200

    Fixed 0x01 for 0x05 on cryptdata for windows rdp

--- server/src/uds/transports/RDP/scripts/windows/tunnel.py
+++ server/src/uds/transports/RDP/scripts/windows/tunnel.py
@@ -23,7 +23,7 @@ tools.addTaskToWait(forwardThread)
 
 # The password must be encoded, to be included in a .rdp file, as 'UTF-16LE' before protecting (CtrpyProtectData) it in order to work with mstsc
 theFile = '''{m.r.as_file}'''.format(
-    password=win32crypt.CryptProtectData(six.binary_type('{m.password}'.encode('UTF-16LE')), None, None, None, None, 0x01).encode('hex'),
+    password=win32crypt.CryptProtectData(six.binary_type('{m.password}'.encode('UTF-16LE')), None, None, None, None, 0x05).encode('hex'),
     address='127.0.0.1:{{}}'.format(port)
 )
 
