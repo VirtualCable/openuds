cce8fa4869dc402655f05fc6a35403988494a083
Adolfo Gómez García <dkmaster@dkmon.com>
2017-05-24 10:23:16 +0200

    Added double Crypt Method authentication

--- server/src/uds/transports/RDP/scripts/windows/direct.py
+++ server/src/uds/transports/RDP/scripts/windows/direct.py
@@ -3,17 +3,24 @@
 from __future__ import unicode_literals
 
 # pylint: disable=import-error, no-name-in-module
-from PyQt4 import QtCore, QtGui
 import win32crypt  # @UnresolvedImport
 import os
 import subprocess
+from uds.log import logger  # @UnresolvedImport
 
 from uds import tools  # @UnresolvedImport
 
 import six
 
+try:
+    thePass = six.binary_type('{m.password}'.encode('UTF-16LE'))
+    password = win32crypt.CryptProtectData(thePass, None, None, None, None, 0x01).encode('hex')
+except Exception:
+    logger.info('Cannot encrypt for user, trying for machine')
+    password = win32crypt.CryptProtectData(thePass, None, None, None, None, 0x05).encode('hex')
+
 # The password must be encoded, to be included in a .rdp file, as 'UTF-16LE' before protecting (CtrpyProtectData) it in order to work with mstsc
-theFile = '''{m.r.as_file}'''.format(password=win32crypt.CryptProtectData(six.binary_type('{m.password}'.encode('UTF-16LE')), None, None, None, None, 0x05).encode('hex'))
+theFile = '''{m.r.as_file}'''.format(password=password)
 
 filename = tools.saveTempFile(theFile)
 executable = tools.findApp('mstsc.exe')
--- server/src/uds/transports/RDP/scripts/windows/tunnel.py
+++ server/src/uds/transports/RDP/scripts/windows/tunnel.py
@@ -4,11 +4,11 @@ from __future__ import unicode_literals
 
 # pylint: disable=import-error, no-name-in-module, too-many-format-args, undefined-variable
 
-from PyQt4 import QtCore, QtGui  # @UnresolvedImport
 import win32crypt  # @UnresolvedImport
 import os
 import subprocess
 from uds.forward import forward  # @UnresolvedImport
+from uds.log import logger  # @UnresolvedImport
 
 from uds import tools  # @UnresolvedImport
 
@@ -21,9 +21,16 @@ if forwardThread.status == 2:
 
 tools.addTaskToWait(forwardThread)
 
+try:
+    thePass = six.binary_type('{m.password}'.encode('UTF-16LE'))
+    password = win32crypt.CryptProtectData(thePass, None, None, None, None, 0x01).encode('hex')
+except Exception:
+    logger.info('Cannot encrypt for user, trying for machine')
+    password = win32crypt.CryptProtectData(thePass, None, None, None, None, 0x05).encode('hex')
+
 # The password must be encoded, to be included in a .rdp file, as 'UTF-16LE' before protecting (CtrpyProtectData) it in order to work with mstsc
 theFile = '''{m.r.as_file}'''.format(
-    password=win32crypt.CryptProtectData(six.binary_type('{m.password}'.encode('UTF-16LE')), None, None, None, None, 0x05).encode('hex'),
+    password=password,
     address='127.0.0.1:{{}}'.format(port)
 )
 
