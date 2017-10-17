214f9c397bbb01e2a32e29fc74c0f8cd0c52357e
Adolfo Gómez García <dkmaster@dkmon.com>
2017-10-17 13:14:56 +0200

    Improved "ensureResponseIsOk" to detect non json responses...

--- server/src/uds/services/OpenGnsys/og/__init__.py
+++ server/src/uds/services/OpenGnsys/og/__init__.py
@@ -75,7 +75,10 @@ def ensureResponseIsValid(response, errMsg=None):
         logger.error('{}: {}'.format(errMsg, response.content))
         raise Exception(errMsg)
 
-    return json.loads(response.content)
+    try:
+        res = json.loads(response.content)
+    except Exception:
+        raise Exception('Error communicating with OpenGnsys: {}'.format(response.content[:128]))
 
 class OpenGnsysClient(object):
     def __init__(self, username, password, endpoint, cache, verifyCert=False):
