8904cc9bc0428a45b99f29a2c248de161f178715
Adolfo Gómez García <dkmaster@dkmon.com>
2018-03-02 05:47:17 +0100

    fixing migrations from 2.2

---
+++ server/src/uds/templatetags/uds.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (c) 2018 Virtual Cable S.L.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright notice,
+#      this list of conditions and the following disclaimer in the documentation
+#      and/or other materials provided with the distribution.
+#    * Neither the name of Virtual Cable S.L. nor the names of its contributors
+#      may be used to endorse or promote products derived from this software
+#      without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+.. moduleauthor:: Adolfo Gómez, dkmaster at dkmon dot com
+"""
+
+from django import template
+from django.utils.translation import ugettext as _
+from django.templatetags.static import static
+from django.utils.html import mark_safe
+
+import json
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+register = template.Library()
+
+
+@register.simple_tag
+def javascript_auths(authenticators):
+    res = []
+    for a in authenticators:
+        theType = a.getType()
+        res.append({
+            'authId': a.uuid,
+            'authSmallName': str(a.small_name),
+            'auth': a.name,
+            'type': theType.typeType,
+            'priority': a.priority,
+            'isCustom': theType.isCustom()
+        })
+    return mark_safe('<script type="text/javascript">\nvar authenticators = ' + json.dumps(res, indent=4) + ';\n</script>');
