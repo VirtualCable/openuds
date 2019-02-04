64faeec13ce6c4c1a00c06c66ebf5d34c934f09d
Adolfo Gómez García <dkmaster@dkmon.com>
2019-02-04 10:00:15 +0100

    Updated translations (machine translation right now) Fixed urls to new format

--- server/src/uds/urls.py
+++ server/src/uds/urls.py
@@ -98,7 +98,7 @@ urlpatterns = [
     re_path(r'^uds/utility/i18n/(?P<lang>[a-z_-]*).js$', JavaScriptCatalog.as_view(), name='utility.jsCatalog'),
     path(r'uds/utility/i18n/', include('django.conf.urls.i18n')),
 
-    # Dowloader
+    # Downloader
     re_path(r'^uds/utility/download/(?P<idDownload>[a-zA-Z0-9-]*)$', uds.web.views.download, name='utility.downloader'),
 
     re_path(r'^uds/utility/files/(?P<uuid>.+)', uds.web.views.file_storage, name='utility.file_storage'),
@@ -129,64 +129,6 @@ urlpatterns = [
 
     # Web admin GUI
     re_path(r'^uds/adm/', include('uds.admin.urls')),
-
-    # old part, remove
-    # re_path(r'^$', uds.web.views.index, name='uds.web.views.index'),
-    # re_path(r'^login/$', uds.web.views.login, name='uds.web.views.login'),
-    # re_path(r'^login/(?P<tag>.+)$', uds.web.views.login, name='uds.web.views.login'),
-    # re_path(r'^logout$', uds.web.views.logout, name='uds.web.views.logout'),
-
-    # Icons
-    # re_path(r'^transicon/(?P<idTrans>.+)$', uds.web.views.transportIcon, name='uds.web.views.transportIcon'),
-    # Images
-    # re_path(r'^srvimg/(?P<idImage>.+)$', uds.web.views.serviceImage, name='uds.web.views.serviceImage'),
-    # re_path(r'^galimg/(?P<idImage>.+)$', uds.web.views.image, name='galleryImage'),
-    # Error URL
-    # re_path(r'^error/(?P<idError>.+)$', uds.web.views.error, name='uds.web.views.error'),
-
-    # Transport own link processor
-    # re_path(r'^trans/(?P<idService>.+)/(?P<idTransport>.+)$', uds.web.views.transportOwnLink, name='TransportOwnLink'),
-    # Authenticators custom html
-    # re_path(r'^customAuth/(?P<idAuth>.*)$', uds.web.views.customAuth, name='uds.web.views.customAuth'),
-    # Preferences
-    # re_path(r'^prefs$', uds.web.views.prefs, name='uds.web.views.prefs'),
-    # Change Language
-    # re_path(r'^i18n/', include('django.conf.urls.i18n')),
-
-    # Downloads
-    # re_path(r'^idown/(?P<idDownload>[a-zA-Z0-9-]*)$', uds.web.views.download, name='uds.web.views.download'),
-
-    # downloads for client
-    # re_path(r'^down$', uds.web.views.client_downloads, name='uds.web.views.client_downloads'),
-    # re_path(r'^down/(?P<os>[a-zA-Z0-9-]*)$', uds.web.views.client_downloads, name='uds.web.views.client_downloads'),
-    # re_path(r'^pluginDetection/(?P<detection>[a-zA-Z0-9-]*)$', uds.web.views.plugin_detection, name='PluginDetection'),
-
-    # Client access enabler
-    # re_path(r'^enable/(?P<idService>.+)/(?P<idTransport>.+)$', uds.web.views.clientEnabler, name='ClientAccessEnabler'),
-
-    # Releaser
-    # re_path(r'^release/(?P<idService>.+)$', uds.web.views.release, name='Releaser'),
-    # re_path(r'^reset/(?P<idService>.+)$', uds.web.views.reset, name='Reseter'),
-
-    # Custom authentication callback
-    # re_path(r'^auth/(?P<authName>.+)', uds.web.views.authCallback, name='uds.web.views.authCallback'),
-    # re_path(r'^authinfo/(?P<authName>.+)', uds.web.views.authInfo, name='uds.web.views.authInfo'),
-    # re_path(r'^about', uds.web.views.about, name='uds.web.views.about'),
-    # Ticket authentication
-    # re_path(r'^tkauth/(?P<ticketId>.+)$', uds.web.views.ticketAuth, name='TicketAuth'),
-
-    # REST Api
-    # re_path(r'^rest/(?P<arguments>.*)$', REST.Dispatcher.as_view(), name="REST"),
-
-    # Web admin GUI
-    # re_path(r'^adm/', include('uds.admin.urls')),
-
-    # Files
-    # re_path(r'^files/(?P<uuid>.+)', uds.web.views.file_storage, name='uds.web.views.file_storage'),
-
-    # Internacionalization in javascript
-    # Javascript catalog. In fact, lang is not used, but it is maintanied for "backward" user templates compatibility
-    # re_path(r'^jsi18n/(?P<lang>[a-z-_]*)$', JavaScriptCatalog.as_view(), name='uds.web.views.jsCatalog'),
 ]
 
 # Append urls from special dispatchers
