68d7ded5bc6e9bbfada349d9c81cacd8637f6dc2
Adolfo Gómez García <dkmaster@dkmon.com>
2021-01-11 03:01:11 +0100

    Upgraded windows script to be compatible with python2.7 & python3 (for future 3.5 client compatibility)

--- server/src/uds/transports/RDP/scripts/windows/direct.py
+++ server/src/uds/transports/RDP/scripts/windows/direct.py
@@ -4,30 +4,33 @@ from __future__ import unicode_literals
 
 # pylint: disable=import-error, no-name-in-module
 import subprocess
-import win32crypt  # @UnresolvedImport
+import win32crypt  # type: ignore
+import codecs
+
 try:
     import winreg as wreg
 except ImportError:  # Python 2.7 fallback
-    import _winreg as wreg  # @UnresolvedImport, pylint: disable=import-error
+    import _winreg as wreg  # type: ignore
 
-from uds.log import logger  # @UnresolvedImport
-from uds import tools  # @UnresolvedImport
+from uds.log import logger  # type: ignore
+from uds import tools  # type: ignore
 
 import six
 
 try:
-    thePass = six.binary_type(sp['password'].encode('UTF-16LE'))  # @UndefinedVariable
-    password = win32crypt.CryptProtectData(thePass, None, None, None, None, 0x01).encode('hex')
+    thePass = six.binary_type(sp['password'].encode('UTF-16LE'))  # type: ignore
+    password = codecs.encode(win32crypt.CryptProtectData(thePass, None, None, None, None, 0x01), 'hex').decode()
 except Exception:
-    logger.info('Cannot encrypt for user, trying for machine')
-    password = win32crypt.CryptProtectData(thePass, None, None, None, None, 0x05).encode('hex')
+    # logger.info('Cannot encrypt for user, trying for machine')
+    password = codecs.encode(win32crypt.CryptProtectData(thePass, None, None, None, None, 0x05), 'hex').decode()
 
 try:
     key = wreg.OpenKey(wreg.HKEY_CURRENT_USER, 'Software\\Microsoft\\Terminal Server Client\\LocalDevices', 0, wreg.KEY_SET_VALUE)  # @UndefinedVariable
     wreg.SetValueEx(key, sp['ip'], 0, wreg.REG_DWORD, 255)  # @UndefinedVariable
     wreg.CloseKey(key)  # @UndefinedVariable
 except Exception as e:
-    logger.warn('Exception fixing redirection dialog: %s', e)
+    # logger.warn('Exception fixing redirection dialog: %s', e)
+    pass  # Key does not exists, ok...
 
 # The password must be encoded, to be included in a .rdp file, as 'UTF-16LE' before protecting (CtrpyProtectData) it in order to work with mstsc
 theFile = sp['as_file'].format(# @UndefinedVariable
--- server/src/uds/transports/RDP/scripts/windows/tunnel.py
+++ server/src/uds/transports/RDP/scripts/windows/tunnel.py
@@ -5,13 +5,15 @@ from __future__ import unicode_literals
 # pylint: disable=import-error, no-name-in-module, too-many-format-args, undefined-variable
 
 import subprocess
-import win32crypt  # @UnresolvedImport
+import win32crypt  # type: ignore
+import codecs
+
 try:
     import winreg as wreg
 except ImportError:  # Python 2.7 fallback
-    import _winreg as wreg  # @UnresolvedImport, pylint: disable=import-error
-from uds.forward import forward  # @UnresolvedImport
-from uds.log import logger  # @UnresolvedImport
+    import _winreg as wreg  # type: ignore
+from uds.forward import forward  # type: ignore
+from uds.log import logger  # type: ignore
 
 from uds import tools  # @UnresolvedImport
 
@@ -26,10 +28,10 @@ tools.addTaskToWait(forwardThread)
 
 try:
     thePass = six.binary_type(sp['password'].encode('UTF-16LE'))  # @UndefinedVariable
-    password = win32crypt.CryptProtectData(thePass, None, None, None, None, 0x01).encode('hex')
+    password = codecs.encode(win32crypt.CryptProtectData(thePass, None, None, None, None, 0x01), 'hex').decode()
 except Exception:
     # Cannot encrypt for user, trying for machine
-    password = win32crypt.CryptProtectData(thePass, None, None, None, None, 0x05).encode('hex')
+    password = codecs.encode(win32crypt.CryptProtectData(thePass, None, None, None, None, 0x05), 'hex').decode()
 
 # The password must be encoded, to be included in a .rdp file, as 'UTF-16LE' before protecting (CtrpyProtectData) it in order to work with mstsc
 theFile = sp['as_file'].format(# @UndefinedVariable
@@ -47,7 +49,8 @@ try:
     wreg.SetValueEx(key, '127.0.0.1', 0, wreg.REG_DWORD, 255)  # @UndefinedVariable
     wreg.CloseKey(key)  # @UndefinedVariable
 except Exception as e:
-    logger.warn('Exception fixing redirection dialog: %s', e)
+    # logger.warn('Exception fixing redirection dialog: %s', e)
+    pass  # Key does not exists, but it's ok
 
 subprocess.Popen([executable, filename])
 tools.addFileToUnlink(filename)
