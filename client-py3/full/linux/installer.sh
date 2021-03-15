034021522d5228a44fdb4f582d56043afbcc4cf8
Adolfo Gómez García <dkmaster@dkmon.com>
2021-03-15 11:25:27 +0100

    Fixed .tar.gz installer info

--- client-py3/full/linux/installer.sh
+++ client-py3/full/linux/installer.sh
@@ -5,9 +5,9 @@ cp -r usr/share/applications /usr/share/applications -R
 update-desktop-database
 
 echo "Installation process done."
-echo "Remembar that the following packages must be installed on system:"
+echo "Remember that the following packages must be installed on system:"
 echo "* Python3 paramiko"
-echo "* Python3 pyqt4"
+echo "* Python3 PyQt5"
 echo "* Python3 requests"
 echo "Theese packages (as their names), are dependent on your platform, so you must locate and install them"
 echo "Also, ensure that a /media folder exists on your machine, that will be redirected on RDP connections"
