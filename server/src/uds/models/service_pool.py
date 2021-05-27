02c3ffbe75bd0feb0c9fe75f6b85418e862f7b1a
Adolfo Gómez García <dkmaster@dkmon.com>
2021-05-27 10:50:17 +0200

    bitarray library previously returned True or False, now return 0 and 1, and we where checking against "is True"

--- server/src/uds/models/service_pool.py
+++ server/src/uds/models/service_pool.py
@@ -256,7 +256,7 @@ class ServicePool(UUIDModel, TaggingMixin):  #  type: ignore
         access = self.fallbackAccess
         # Let's see if we can access by current datetime
         for ac in sorted(self.calendarAccess.all(), key=lambda x:x.priority):
-            if CalendarChecker(ac.calendar).check(chkDateTime) is True:
+            if CalendarChecker(ac.calendar).check(chkDateTime):
                 access = ac.access
                 break  # Stops on first rule match found
 
