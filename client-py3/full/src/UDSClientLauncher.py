09e88b60f59531c33b955f4062e8613d0e89d5cf
Adolfo Gómez García <dkmaster@dkmon.com>
2021-07-03 12:30:46 +0200

    Updated launcher so, if launcher is closed, all tunnels are also closed

--- client-py3/full/src/UDSClientLauncher.py
+++ client-py3/full/src/UDSClientLauncher.py
@@ -13,14 +13,28 @@ SCRIPT_NAME = 'UDSClientLauncher'
 
 class UdsApplication(QtWidgets.QApplication):
     path: str
+    tunnels: typing.List[subprocess.Popen]
+
     def __init__(self, argv: typing.List[str]) -> None:
         super().__init__(argv)
         self.path = os.path.join(os.path.dirname(sys.argv[0]).replace('Resources', 'MacOS'), SCRIPT_NAME)
+        self.tunnels = []
+        self.lastWindowClosed.connect(self.closeTunnels)  # type: ignore
+
+    def cleanTunnels(self) -> None:
+        for k in [i for i, tunnel in enumerate(self.tunnels) if tunnel.poll() is not None]:
+            del self.tunnels[k]
+
+    def closeTunnels(self) -> None:
+        logger.debug('Closing remaining tunnels')
+        for tunnel in self.tunnels:
+            if tunnel.poll() is None:  # Running
+                logger.info('Found running tunnel %s, closing it', tunnel.pid)
+                tunnel.kill()
 
     def event(self, evnt: QtCore.QEvent) -> bool:
-        logger.debug('Got event %s -> %s', evnt, evnt.type())
-
         if evnt.type() == QtCore.QEvent.FileOpen:
+            # First, remove all finished tunnel processed from check queue
             fe = typing.cast(QtGui.QFileOpenEvent, evnt)
             logger.debug('Got url: %s', fe.url().url())
             fe.accept()
