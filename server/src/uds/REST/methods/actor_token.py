99ee0b00fc54fd25c0f1310ad45327e4b5430543
Adolfo Gómez García <dkmaster@dkmon.com>
2021-07-09 13:13:31 +0200

    Added actor token to admin

--- server/src/uds/REST/methods/actor_token.py
+++ server/src/uds/REST/methods/actor_token.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Copyright (c) 2020 Virtual Cable S.L.U.
+# Copyright (c) 2021 Virtual Cable S.L.U.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
