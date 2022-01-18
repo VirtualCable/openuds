f702c144fc8937d2ffaa14f469f0613bd21a627b
Adolfo Gómez García <dkmaster@dkmon.com>
2022-01-18 13:34:33 +0100

    small thinpro fix

--- client-py3/full/linux/thinpro/install-uds-client.sh
+++ client-py3/full/linux/thinpro/install-uds-client.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+# Common part
+
 # unlocks so we can write on TC
 fsunlock
-# Common part
+
 cp UDSClient /bin/udsclient
 chmod 755 /bin/udsclient
-cp UDSClient.desktop /usr/share/applications/UDSClient.desktop
 # RDP Script for UDSClient. Launchs udsclient using the "Template_UDS" profile
 cp udsrdp /usr/bin
 
@@ -13,7 +14,7 @@ INSTALLED=0
 # Installation for 7.1.x version
 grep -q "7.1" /etc/issue
 if [ $? -eq 0 ]; then
-    echo "Installing for 7.1 version"
+    echo "Installing for thinpro version 7.1"
     # Allow UDS apps without asking
     cp firefox7.1/syspref.js /etc/firefox
     # Copy handlers.json for firefox
@@ -21,9 +22,8 @@ if [ $? -eq 0 ]; then
     cp firefox7.1/handlers.json /lib/UDSClient/firefox/
     # and runner
     cp firefox7.1/45-uds /etc/hptc-firefox-mgr/prestart
-    INSTALLED=1
 else
-    echo "Installing for > 7.1 version"
+    echo "Installing for thinpro version 7.2 or later"
     # Copy handlers for firefox
     mkdir -p /lib/UDSClient/firefox/ > /dev/null 2>&1
     # Copy handlers.json for firefox
@@ -32,13 +32,7 @@ else
     # copy uds handler for firefox
     cp firefox/uds /usr/share/hptc-firefox-mgr/handlers/uds
     chmod 755 /usr/share/hptc-firefox-mgr/handlers/uds
-    INSTALLED=1
 fi
 
-# If not installed, show a message
-if [ $INSTALLED -eq 0 ]; then
-    echo "UDSClient is not installable for this version of ThinPro: "
-    cat /etc/issue
-fi
-
+# Common part
 fslock
