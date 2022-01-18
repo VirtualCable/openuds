ce2d2b1c2e8073195ba1580535a2100c5aeded6b
Adolfo Gómez García <dkmaster@dkmon.com>
2022-01-18 13:18:24 +0100

    added installer for thinpro 7.2

--- client-py3/full/linux/thinpro/install-uds-client.sh
+++ client-py3/full/linux/thinpro/install-uds-client.sh
@@ -13,12 +13,25 @@ INSTALLED=0
 # Installation for 7.1.x version
 grep -q "7.1" /etc/issue
 if [ $? -eq 0 ]; then
+    echo "Installing for 7.1 version"
     # Allow UDS apps without asking
     cp firefox7.1/syspref.js /etc/firefox
-    # Copy handlers for firefox
+    # Copy handlers.json for firefox
     mkdir -p /lib/UDSClient/firefox/ > /dev/null 2>&1
     cp firefox7.1/handlers.json /lib/UDSClient/firefox/
+    # and runner
     cp firefox7.1/45-uds /etc/hptc-firefox-mgr/prestart
+    INSTALLED=1
+else
+    echo "Installing for > 7.1 version"
+    # Copy handlers for firefox
+    mkdir -p /lib/UDSClient/firefox/ > /dev/null 2>&1
+    # Copy handlers.json for firefox
+    cp firefox/handlers.json /lib/UDSClient/firefox/
+    cp firefox/45-uds /etc/hptc-firefox-mgr/prestart
+    # copy uds handler for firefox
+    cp firefox/uds /usr/share/hptc-firefox-mgr/handlers/uds
+    chmod 755 /usr/share/hptc-firefox-mgr/handlers/uds
     INSTALLED=1
 fi
 
