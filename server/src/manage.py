a068838e0eaeed6b70486c1ca3692a97cb0a8a40
Adolfo Gómez García <dkmaster@dkmon.com>
2022-11-02 02:58:54 +0100

    removec cProfile from manage.py

--- server/src/manage.py
+++ server/src/manage.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 import os
 import sys
-import cProfile
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
     from django.core.management import execute_from_command_line
 
-    cProfile.run("execute_from_command_line(sys.argv)", "/tmp/django.pyprof")
+    execute_from_command_line(sys.argv)", "/tmp/django.pyprof
