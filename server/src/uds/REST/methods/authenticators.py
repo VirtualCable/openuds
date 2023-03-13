a12aa1f3d42a2e454cde07c962d459dcc52dc31f
Adolfo Gómez García <dkmaster@dkmon.com>
2023-03-13 18:05:18 +0100

    fixed auth label re

--- server/src/uds/REST/methods/authenticators.py
+++ server/src/uds/REST/methods/authenticators.py
@@ -228,7 +228,7 @@ class Authenticators(ModelHandler):
 
         fields['small_name'] = fields['small_name'].strip().replace(' ', '-')
         # And ensure small_name chars are valid [ a-zA-Z0-9:-.]+
-        if fields['small_name'] and not re.match(r'^[a-zA-Z0-9:-.]+$', fields['small_name']):
+        if fields['small_name'] and not re.match(r'^[a-zA-Z0-9:.-]+$', fields['small_name']):
             raise self.invalidRequestException(
                 _('Label must contain only letters, numbers, or symbols: - : .')
             )
