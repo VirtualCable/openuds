a4a07be77f638bd1002241817a1bd171e6c2be8d
Adolfo Gómez García <dkmaster@dkmon.com>
2024-08-29 16:14:42 +0200

    chore: Update LinuxOsADManager type_name to 'Linux Active Directory OS Manager'

--- server/src/uds/osmanagers/LinuxOsManager/linux_ad_osmanager.py
+++ server/src/uds/osmanagers/LinuxOsManager/linux_ad_osmanager.py
@@ -49,7 +49,7 @@ logger = logging.getLogger(__name__)
 
 
 class LinuxOsADManager(LinuxOsManager):
-    type_name = _('Linux OS Active Directory Manager')
+    type_name = _('Linux Active Directory OS Manager')
     type_type = 'LinuxADManager'
     type_description = _('Os Manager to control Linux virtual machines with active directory')
     icon_file = 'losmanager.png'
