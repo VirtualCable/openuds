7fa3d494e27a5d16653bbbfdb5f4824e3d5f9a08
Adolfo Gómez García <dkmaster@dkmon.com>
2024-11-08 16:11:09 +0100

    Refactor user service to filter enabled pool members directly in query

--- server/src/uds/core/managers/user_service.py
+++ server/src/uds/core/managers/user_service.py
@@ -990,7 +990,7 @@ class UserServiceManager(metaclass=singleton.Singleton):
 
         # Get pool members. Just pools "visible" and "usable"
         poolMembers = [
-            p for p in meta.members.all() if p.pool.isVisible() and p.pool.isUsable() and p.enabled
+            p for p in meta.members.filter(enabled=True) if p.pool.isVisible() and p.pool.isUsable()
         ]
         # Sort pools based on meta selection
         if meta.policy == MetaPool.PRIORITY_POOL:
