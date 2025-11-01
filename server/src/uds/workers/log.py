335c1a8c056dd011ec7139672e7d2df1b8bc3fa7
Adolfo Gómez García <dkmaster@dkmon.com>
2025-11-01 04:59:04 +0100

    Refactor log maintenance logic to improve clarity and efficiency in log deletion process

--- server/src/uds/workers/log.py
+++ server/src/uds/workers/log.py
@@ -31,6 +31,7 @@ Author: Adolfo Gómez, dkmaster at dkmon dot com
 """
 import datetime
 import logging
+import typing
 
 from django.db.models import Count
 
@@ -41,18 +42,18 @@ from uds.core.util.model import sql_now
 from uds.core.util import config
 
 # from uds.core.util.config import GlobalConfig
-
+MAX_BATCH_SIZE: typing.Final[int] = 32768
 
 logger = logging.getLogger(__name__)
 
 
 class LogMaintenance(Job):
-    frecuency = 3600  # Once every hour
+    frecuency = 30  # Once every hour
     # frecuency_cfg = GlobalConfig.XXXX
     friendly_name = 'Log maintenance'
 
     def run(self) -> None:
-        logger.debug('Starting log maintenance')
+        logger.debug('Starting Log maintenance')
         # Select all disctinct owner_id and owner_type and count of each
         # For each one, check if it has more than max_elements, and if so, delete the oldest ones
         for owner_id, owner_type, count in (
@@ -69,36 +70,26 @@ class LogMaintenance(Job):
                 continue
 
             max_elements = owner_type.get_max_elements()
-            # Ensures that last hour logs are not deleted
-            removing_before = sql_now() - datetime.timedelta(seconds=3600)
+
             if 0 < max_elements < count:  # Negative max elements means "unlimited"
-                # Recent logs (last hour)
-                recent_ids = set(
+                logger.debug(
+                    'Log maintenance: Owner %s of type %s has %d logs, max is %d, cleaning up',
+                    owner_id,
+                    owner_type.name,
+                    count,
+                    max_elements,
+                )
+                ids_to_delete = list(
                     models.Log.objects.filter(
-                        owner_id=owner_id, owner_type=owner_type, created__gte=removing_before
-                    ).values_list('id', flat=True)
+                        owner_id=owner_id,
+                        owner_type=owner_type,
+                    )
+                    .order_by('created', 'id')
+                    .values_list('id', flat=True)[max_elements : max_elements + MAX_BATCH_SIZE]
                 )
 
-                num_to_keep = max_elements - len(recent_ids)
-                old_ids_to_keep: set[int] = set()
-                if num_to_keep > 0:
-                    # Old logs (before last hour)
-                    old_ids_to_keep = set(
-                        models.Log.objects.filter(
-                            owner_id=owner_id, owner_type=owner_type, created__lt=removing_before
-                        )
-                        .order_by('-created', '-id')
-                        .values_list('id', flat=True)[:num_to_keep]
-                    )
-
-                # IDs to keep
-                ids_to_keep = recent_ids.union(old_ids_to_keep)
-
-                # Delete everything else
-                models.Log.objects.filter(
-                    owner_id=owner_id,
-                    owner_type=owner_type,
-                ).exclude(id__in=ids_to_keep).delete()
+                if ids_to_delete:
+                    models.Log.objects.filter(id__in=ids_to_delete).delete()
 
         # Also, delete all logs older than config.GlobalConfig.STATS_DURATION.as_int()*2 days
         # This is to ensure we do not have "orphan" logs too old
