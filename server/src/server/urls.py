6c6e2f64175dace66ce621ab31942c9de97db093
Adolfo Gómez García <dkmaster@dkmon.com>
2025-11-12 00:48:47 +0100

    Remove server folder, preparing for submodule

--- server/src/server/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Url patterns for UDS project (Django)
-"""
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('uds.urls')),
-]
--- server/src/uds/core/util/AutoAttributes.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Allow old serialized autoattributes to load correctly (register path...)
-
-# pylint: disable=unused-wildcard-import,wildcard-import
-from .auto_attributes import *
