61eed86ba0334be7af19125cff3945764a2b8b0f
Adolfo Gómez García <dkmaster@dkmon.com>
2026-06-23 15:35:27 +0200

    fix(RDP): update condition for 'as_file' parameter to include mac_allow_msrdc

--- server/src/uds/transports/RDP/rdptunnel.py
+++ server/src/uds/transports/RDP/rdptunnel.py
@@ -239,7 +239,7 @@ class TRDPTransport(BaseRDPTransport):
             sp.update(
                 {
                     'as_new_xfreerdp_params': r.as_new_xfreerdp_params,
-                    'as_file': r.as_file if self.mac_use_rdp_file.as_bool() else '',
+                    'as_file': r.as_file if self.mac_allow_msrdc.as_bool() or self.mac_use_rdp_file.as_bool() else '',
                     'as_rdp_url': r.as_rdp_url if self.mac_allow_msrdc.as_bool() else '',
                 }
             )
